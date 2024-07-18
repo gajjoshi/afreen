801f10bad224d2effb11c06a8128450b6cf512a7
masterbhuvan1 <bhuvanjain212003@gmail.com>
2024-07-18 23:05:10 +0530

    new

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-// const BACKEND_URL = "https://afreen.onrender.com;
+const BACKEND_URL = "https://afreen.onrender.com";
 // const BACKEND_URL = "http://localhost:3002";
 
 export default BACKEND_URL;
